cd33fc0c80f690491801f9913c147279120b6d36
nkemjikaa <51340029+nkemjikaa@users.noreply.github.com>
2021-07-09 13:44:07 +0100

    Create helloworld.php

---
+++ helloworld.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<title>Hello World</title>
+</head>
+	<p>HTML says Hello</p>
+		<script type="text/javascript">
+		document.write('<p>Javascript says Hello</p>');
+		</script>
+			<?php
+			echo "<p>PHP says Hello</p>";
+		?>
+	<body>
+	</body>
+	</html>
